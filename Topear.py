5b05f7c4686f359e69522ca2ef7570c87c638101
AlbertoGomezdeAndres <151046210+AlbertoGomezdeAndres@users.noreply.github.com>
2023-12-02 18:08:16 +0100

    Add files via upload

---
+++ Topear.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, desc, round as spark_round
+
+sc = SparkSession.builder.appName("Topear").getOrCreate()
+
+path = "beeradvocate_note.json"
+
+df = sc.read.json(path)
+
+#Agrupar por nombre y sacar nota media con todas las reviews
+df_con_medias = df.groupBy("beer/name").agg(spark_round(avg("review/note"), 2).alias("note_average"))
+
+#Ordenar por la nota media
+df_ordenado = df_con_medias.orderBy(desc("note_average"))
+
+#Atributos que queremos mostrar
+df_final = df_ordenado.select("beer/name", "note_average")
+
+df_final.show()
+
+sc.stop()
