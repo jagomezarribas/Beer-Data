f2a08e1509e4a7236265bae309371a7ef60f71cb
AlbertoGomezdeAndres <151046210+AlbertoGomezdeAndres@users.noreply.github.com>
2023-12-10 12:02:56 +0100

    Update Topear_cervezas_por_rangos.py

--- code/Topear_cervezas_por_rangos.py
+++ code/Topear_cervezas_por_rangos.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, floor, avg, desc, round as spark_round
-import sys
+from pyspark.sql.functions import avg, desc, round as spark_round
 
 #Creamos una sesión de Spark
 sc = SparkSession.builder.appName("Rangos_ABV").getOrCreate()
@@ -13,11 +12,12 @@ df = sc.read.json(path)
 df_notas_medias = df.groupBy("beer/name").agg(spark_round(avg("review/note"), 2).alias("beers_average"))
 
 #Ordenamos por la nota media de las cervezas en orden descendente
-df_final = df_notas_medias.orderBy(desc("beers_average"))
+df_final = df_notas_medias.orderBy(("beers_average"))
 
 #Creamos una tabla por cada rango en la nota media
 for i in range(5):
-    df_fx = df_final.filter((df_final['beers_average'] >= i) & (df_final['beers_average'] <= i+1))
+    df_fx = df_final.filter((df_final['beers_average'] > i) & (df_final['beers_average'] <= i+1))
+    df_fx.coalesce(1).write.options(header = 'True', delimiter = '  ').mode("overwrite").csv("../CSV's/Topear_cervezas_por_rangos/" + "Rango " + str(i+1))
     df_fx.show(df_fx.count(), truncate=False)
 
 sc.stop()
